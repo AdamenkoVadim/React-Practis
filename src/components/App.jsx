c001e8dd98d6670ebddf4c881aa6e9ac1d0707d2
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-04-02 18:05:05 +0200

    Comm204-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,53 @@
+import React,{ Component } from 'react';
 /* import Profile from './Profile/profile';
 import user from './Profile/user.json';
 import Counter from './Counter/Counter' */
-import Dropdown from './Dropdown/Dropdown';
-export const App = () => {
-  return (
-    <div>
-    {/*   {user.map(user => (
-       <Profile
-          key={user.id}
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-      />
+/* import Dropdown from './Dropdown/Dropdown'; */
+import TodoList from './TodoList';
+import initialTodos from 'components/TodoList/todos.json'
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  }
+
+deleteTodo = (todoId) => {
+  this.setState(prevState => ({
+    todos: prevState.todos.filter(todo => todo.id !== todoId),
+  }))
+}
+
+  render() {
+    const { todos } = this.state;
+    const comletedTodos = todos.filter(todo => todo.completed);
+    console.log(comletedTodos.length);
+    return (
+      <>
+      {/*   {user.map(user => (
+         <Profile
+            key={user.id}
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+        
+        ))}
+        <Counter /> */}
+        {/* <Dropdown /> */}
+        <h1>Состояние компонента</h1>
+        <p>Total ToDos: {todos.length}</p>
+        <p>Total Dan ToDos:{comletedTodos.length}</p>
+        <TodoList 
+        todos={todos}
+        onDeleteTodo = {this.deleteTodo}
+        />
+      </>
       
-      ))}
-      <Counter /> */}
-      <Dropdown />
-    </div>
-    
-   
-  );
-};
+     
+    );
+  }
+}
+ 
+export default App;
